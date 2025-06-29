bcde550d610f33b7afdc356c9e75fec1a92e7b41
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2025-06-29 09:34:41 -0600

    update versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 # terraform/main.tf
 
 terraform {
-  required_version = ">= 1.7.0"
+  required_version = ">= 1.9.0"
   
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.82"
     }
   }
   
