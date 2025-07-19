e52f48bfa6b63d9d626f38a040f4608ba67baecc
john-matlock-eng <91761832+john-matlock-eng@users.noreply.github.com>
2025-07-19 11:41:37 -0600

    ellie

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -8,3 +8,7 @@ export { default as DevTools } from "./DevTools";
 export { default as LoadingScreen } from "./LoadingScreen";
 export { default as ThemeSwitcher } from "./ThemeSwitcher";
 export { Confetti } from "./Confetti";
+
+export { default as AnimatedShihTzu } from "./AnimatedShihTzu";
+export type { AnimatedShihTzuProps } from "./AnimatedShihTzu";
+export { default as ShihTzuCompanionExample } from "./ShihTzuCompanionExample";
